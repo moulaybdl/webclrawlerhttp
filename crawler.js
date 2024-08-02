c2a9dbf93546f3e657222e68e046cc27852c2c24
moulaybdl <moulay.mohamed.bouabd.elli@ensia.edu.dz>
2024-08-02 02:23:46 +0200

    Adding lowercase handling

--- crawler.js
+++ crawler.js
@@ -1,11 +1,11 @@
 const url = require("node:url");
 function normalizeURL(urlStr) {
   const urlObj = url.parse(urlStr);
-  var result =
-    urlObj.pathname.charAt(urlObj.pathname.length - 1) == "/"
-      ? urlObj.pathname
-      : urlObj.pathname + "/";
-  return `${urlObj.hostname}${result}`;
+  var result = `${urlObj.hostname}${urlObj.pathname}`;
+  if (result.length > 0 && result[-1] === "/") {
+    result = result.slice(0, -1);
+  }
+  return result.toLowerCase();
 }
 
 module.exports = { normalizeURL };
